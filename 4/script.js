92b74eb1874bce41595aa64dc28aae6196e4869b
George Glocker <georgeglocker@gmail.com>
2022-12-04 23:20:30 +0400

    Update script.js

--- 4/script.js
+++ 4/script.js
@@ -11,14 +11,24 @@ function part1() {
 
         // Getting the start value and end value of each pair of numbers
         const [startID, endID] = line.split(",")
-                                     .map((pairs) =>
-                                         pairs.split("-")
-                                              .map(Number));
+                                     .map((pairs) => pairs
+                                     .split("-")
+                                     .map(Number))
+                                     .sort((a, b) => {
+                                         const sameLength = a[1] - a[0];
+                                         const differentLength = b[1] - b[0];
 
-        console.log({startID, endID});
+                                         return differentLength - sameLength;
+                                     })
+        // Comparing numbers in sections
+        const firstIncludesSecond =
+            startID[0] <= endID[0] && startID[1] >= endID[1];
+
+        return firstIncludesSecond ? 1 : 0;
+
     });
-
-    console.log(sectionsList);
+    // Summing all includes
+    console.log(sectionsList.reduce((a, b) => a + b, 0));
 }
 
 part1();
